fc47ba1c4539df5958ada5342612cadbba9c6260
Viktoriya Andriyanova <vika.andriyanova.01@bk.ru>
2026-01-17 23:49:32 +0300

    module2-task1: созданы основные React-компоненты

---
+++ src/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from '../pages/main-page/main-page';
+
+interface AppProps {
+  offersCount: number;
+}
+
+function App({ offersCount }: AppProps) {
+  return <MainPage offersCount={offersCount} />;
+}
+
+export default App;
